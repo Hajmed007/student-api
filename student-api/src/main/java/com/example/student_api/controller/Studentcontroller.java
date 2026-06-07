899904aa3493ee3cf3e46a6319bc92336bc2e5bb
Mohamed Hajmed Ali <167395448+Hajmed007@users.noreply.github.com>
2026-06-07 17:43:28 +0530

    Initial commit

---
+++ student-api/src/main/java/com/example/student_api/controller/Studentcontroller.java
@@ -0,0 +1,38 @@
+package com.example.student_api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.student_api.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class Studentcontroller {
+    ArrayList<Student> list = new ArrayList<>();
+
+    @PostMapping("/add")
+    public String addstd(@RequestBody Student std)
+    {
+      list.add(std);
+      return "Student Added Sucessfully";
+    }
+    @GetMapping("/get")
+    public List<Student> getstd()
+    {
+        return list;
+    }
+    @DeleteMapping("/delete/{id}")
+    public String deletestd(@PathVariable int id)
+    {
+        list.remove(id);
+         return "Student {id} Deleted ";
+    }
+}
---
+++ student-api/src/main/java/com/example/student_api/entity/Student.java
@@ -0,0 +1,18 @@
+package com.example.student_api.entity;
+
+
+public class Student {
+    int id;
+    String name;
+    String email;
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    
+}
