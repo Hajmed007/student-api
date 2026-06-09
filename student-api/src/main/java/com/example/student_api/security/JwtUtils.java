c20e48d0f67ed607865a45c5669ce9e296518846
Mohamed Hajmed Ali <167395448+Hajmed007@users.noreply.github.com>
2026-06-09 22:21:59 +0530

    feat: add JwtUtil class SEC-101

---
+++ student-api/src/main/java/com/example/student_api/security/JwtUtils.java
@@ -0,0 +1,34 @@
+package com.example.student_api.security;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtils {
+
+    public String generateToken(String name)
+    {
+       return Jwts.builder()
+                  .setSubject(name)
+                  .setExpiration(new Date(System.currentTimeMillis + 3600000))
+                  .signWith(SignatureAlgorithm.HS384, "secretkey")
+                  .compact();
+    }
+    public String extractUsername(String token)
+    {
+        return Jwts.parser()
+                   .setSigningKey("secretkey")
+                   .parseClaimsJws(token)
+                   .getBody()
+                   .getSubject();
+    }
+    public boolean validate(String token,String name)
+    {
+        return extractUsername(token).equals(name);
+    }
+    
+} 
